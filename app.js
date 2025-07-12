a2e4aab72ee877f5dac4887dfd42f714aafd8d97
spmcbth <machgiahan3624@gmail.com>
2025-07-13 00:48:38 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -14,4 +14,19 @@ app.get("/", (req, res) => {
 
 app.use("/api/contacts", contactRouter);
 
+// Handle 404 response
+app.use((req, res, next) => {
+    // Gọi next() để chuyển sang middleware xử lý lỗi
+    // khi không có route nào được định nghĩa khớp với yêu cầu
+    return next(new ApiError(404, "Resource not found"));
+});
+
+// Middleware xử lý lỗi tập trung
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
+
+
 module.exports = app;
